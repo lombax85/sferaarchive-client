c501d66e6961f546f10972282e1ec59b52a24d34
Fabio Lombardo <lombax85@gmail.com>
2024-06-30 01:58:40 +0200

    header + user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
-import ChannelList from './ChannelList';
-import MessageList from './MessageList';
-import SearchBar from './SearchBar';
-import Thread from './Thread';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import ChannelList from "./ChannelList";
+import MessageList from "./MessageList";
+import SearchBar from "./SearchBar";
+import Thread from "./Thread";
+import { useLocation } from "react-router-dom";
 
 function App() {
-
-  const API_URL = 'https://slack-archive.sferait.org';
+  const API_URL = "https://slack-archive.sferait.org";
 
   const [channels, setChannels] = useState([]);
   const [selectedChannel, setSelectedChannel] = useState(null);
@@ -18,41 +17,56 @@ function App() {
   const [selectedThread, setSelectedThread] = useState(null);
   const [threadMessages, setThreadMessages] = useState([]);
   const [accessToken, setAccessToken] = useState(null);
-
+  const [user, setUser] = useState(null);
+  const [optedOut, setOptedOut] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
-    const token = queryParams.get('token');
+    const token = queryParams.get("token");
     if (token) {
       setAccessToken(token);
     } else {
-      window.location.href = API_URL + '/login';
+      window.location.href = API_URL + "/login";
     }
   }, [location]);
 
-
   useEffect(() => {
     // fetch the channels
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-    axios.get(API_URL + '/channels')
-      .then(response => setChannels(response.data))
-      .catch(error => console.error('Error fetching channels:', error));
+    axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    axios
+      .get(API_URL + "/channels")
+      .then((response) => setChannels(response.data))
+      .catch((error) => console.error("Error fetching channels:", error));
+
+    axios
+      .get(API_URL + "/whoami")
+      .then((response) => { 
+        setUser(response.data.user_id)
+        setOptedOut(response.data.opted_out) 
+      })
+      .catch((error) => console.error("Error fetching whoami:", error));
   }, [accessToken]);
 
   useEffect(() => {
     if (selectedChannel) {
-      axios.get(`${API_URL}/messages/${selectedChannel}?offset=${offset}`)
-        .then(response => setMessages(prevMessages => [...prevMessages, ...response.data]))
-        .catch(error => console.error('Error fetching messages:', error));
+      axios
+        .get(`${API_URL}/messages/${selectedChannel}?offset=${offset}`)
+        .then((response) =>
+          setMessages((prevMessages) => [...prevMessages, ...response.data])
+        )
+        .catch((error) => console.error("Error fetching messages:", error));
     }
   }, [selectedChannel, offset]);
 
   useEffect(() => {
     if (selectedThread) {
-      axios.get(`${API_URL}/thread/${selectedThread}`)
-        .then(response => setThreadMessages(response.data))
-        .catch(error => console.error('Error fetching thread messages:', error));
+      axios
+        .get(`${API_URL}/thread/${selectedThread}`)
+        .then((response) => setThreadMessages(response.data))
+        .catch((error) =>
+          console.error("Error fetching thread messages:", error)
+        );
     }
   }, [selectedThread]);
 
@@ -64,7 +78,7 @@ function App() {
 
   const handleScroll = (e) => {
     if (e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight) {
-      setOffset(prevOffset => prevOffset + 20);
+      setOffset((prevOffset) => prevOffset + 20);
     }
   };
 
@@ -74,18 +88,27 @@ function App() {
 
   return (
     <div className="App">
-      <ChannelList channels={channels} onSelect={handleChannelSelect} />
-      <div className="message-container" onScroll={handleScroll}>
-        <SearchBar setMessages={setMessages} />
-        <MessageList messages={messages} onThreadSelect={handleThreadSelect} />
-         </div>
-         {selectedThread && (
-           <div className="thread-container">
-             <Thread messages={threadMessages} />
-           </div>
-         )}
-       </div>
-     );
-   }
+      <div className="header">
+        User: {user} &nbsp;
+        Opted Out: {optedOut ? "Yes" : "No"}
+      </div>
+      <div className="main-content">
+        <ChannelList channels={channels} onSelect={handleChannelSelect} />
+        <div className="message-container" onScroll={handleScroll}>
+          <SearchBar setMessages={setMessages} />
+          <MessageList
+            messages={messages}
+            onThreadSelect={handleThreadSelect}
+          />
+        </div>
+        {selectedThread && (
+          <div className="thread-container">
+            <Thread messages={threadMessages} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
-   export default App;
+export default App;
