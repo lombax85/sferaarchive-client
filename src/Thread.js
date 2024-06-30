b5f3e56c9b048af5368fb7fc575b006f256069fb
Fabio Lombardo <lombax85@gmail.com>
2024-06-30 17:29:27 +0200

    markdown & links formatting

--- src/Thread.js
+++ src/Thread.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { marked } from 'marked';
+import parse from 'html-react-parser';
 
 function Thread({ messages }) {
   return (
     <div className="thread">
       {messages.map(message => (
         <div key={message.timestamp} className="message">
-          <p><strong>{message.user_name}</strong>: {message.message}</p>
+           <p><strong>{message.user_name}</strong>:</p>
+          <div>{parse(marked(message.message))}</div>
           <span>{new Date(parseFloat(message.timestamp) * 1000).toLocaleString()}</span>
           &nbsp;
-          { message.permalink && 
-            <a target="_blank" href={`${message.permalink}`}>Permalink</a>
+          {message.permalink &&
+            <a target="_blank" rel="noopener noreferrer" href={`${message.permalink}`}>Permalink</a>
           }
         </div>
       ))}
